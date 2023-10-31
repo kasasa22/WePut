e460d4a878b035b4eb767563589e681ef1b719d3
Ambrose Alanda <alandaambrose@gmail.com>
2023-10-31 23:25:35 +0300

    send

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
@@ -10,7 +11,40 @@ class HomePage extends StatelessWidget {
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: const Text("Home Page"),
         centerTitle: true,
+        actions: [
+          IconButton(
+            onPressed: () {
+              //sign out the user
+              FirebaseAuth.instance.signOut();
+            },
+            icon: const Icon(Icons.logout),
+          ),
+        ],
       ),
+      drawer: Drawer(
+        child: ListView(
+          children: [
+            const DrawerHeader(
+              decoration: BoxDecoration(
+                color: Colors.blue,
+              ),
+              child: Text("Drawer Header"),
+            ),
+            ListTile(
+              title: const Text("Item 1"),
+              onTap: () {},
+            ),
+            ListTile(
+              title: const Text("Item 2"),
+              onTap: () {},
+            ),
+            ListTile(
+              title: const Text("Item 3"),
+              onTap: () {},
+            ),
+          ],
+        ),
+      )
       body: const Center(
         child: Text("Home Page"),
       ),
