c7a6638342d1495d36528248f582dad01b8b2b9f
Ambrose Alanda <alandaambrose@gmail.com>
2024-01-05 13:02:51 +0000

    Jan 5, 2024, 4:02 PM

--- lib/pages/crud_test.dart
+++ lib/pages/crud_test.dart
@@ -7,7 +7,7 @@ class Test extends StatefulWidget {
   const Test({Key? key}) : super(key: key);
 
   @override
-  _TestState createState() => _TestState();
+  State<Test> createState() => _TestState();
 }
 
 class _TestState extends State<Test> {
@@ -19,66 +19,34 @@ class _TestState extends State<Test> {
       appBar: AppBar(
         title: const Text('CRUD Test'),
       ),
-      body: StreamBuilder<List<Task>>(
-        stream: taskService.getTasksStream(),
-        builder: (context, snapshot) {
-          if (snapshot.hasError) {
-            print('Error: ${snapshot.error}');
-            return const Text('Error loading tasks');
-          }
+      body: 
 
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return const CircularProgressIndicator();
-          }
-
-          if (!snapshot.hasData || snapshot.data!.isEmpty) {
-            return const Center(
-              child: Text('No tasks available.'),
-            );
-          }
-
-          return ListView.builder(
-            itemCount: snapshot.data!.length,
-            itemBuilder: (context, index) {
-              var task = snapshot.data![index];
-              return ListTile(
-                title: Text(task.title),
-                subtitle: Text(task.description),
-                trailing: IconButton(
-                  icon: const Icon(Icons.delete),
-                  onPressed: () => _deleteTask(task.taskId),
-                ),
-              );
-            },
-          );
-        },
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () => _addTask(),
-        child: const Icon(Icons.add),
-      ),
+      // floatingActionButton: FloatingActionButton(
+      //   onPressed: () => _addTask(),
+      //   child: const Icon(Icons.add),
+      // ),
     );
   }
 
-  Future<void> _addTask() async {
-    await taskService.addTask(Task(
-      taskId: '',
-      title: 'New Task',
-      description: 'Description for the new task.',
-      dueDate: Timestamp.now(),
-      status: 'pending',
-      assignedUserId: 'someUserId',
-      priority: 'medium',
-      category: 'General',
-      progress: 0,
-      comments: [],
-      startTime: Timestamp.now(),
-      endTime: Timestamp.now(),
-      evaluation: 0.0,
-    ));
-  }
+  // Future<void> _addTask() async {
+  //   await taskService.addTask(Task(
+  //     taskId: '',
+  //     title: 'New Task',
+  //     description: 'Description for the new task.',
+  //     dueDate: Timestamp.now(),
+  //     status: 'pending',
+  //     assignedUserId: 'someUserId',
+  //     priority: 'medium',
+  //     category: 'General',
+  //     progress: 0,
+  //     comments: [],
+  //     startTime: Timestamp.now(),
+  //     endTime: Timestamp.now(),
+  //     evaluation: 0.0,
+  //   ));
+  // }
 
-  Future<void> _deleteTask(String taskId) async {
-    await taskService.deleteTask(taskId);
-  }
+  // Future<void> _deleteTask(String taskId) async {
+  //   await taskService.deleteTask(taskId);
+  // }
 }
--- lib/services/task.dart
+++ lib/services/task.dart
@@ -1,29 +1,45 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import '../models/task.dart';
 
+// ignore: constant_identifier_names
+const String COLLECTION_REF = "tasks";
+
 class TaskService {
-  final CollectionReference tasksCollection =
-      FirebaseFirestore.instance.collection('tasks');
+  final _firestore = FirebaseFirestore.instance;
 
-  Future<void> addTask(Task task) async {
-    await tasksCollection.add(task.toJson());
+  // ignore: unused_field
+  late final CollectionReference _taskRef;
+
+  TaskService() {
+    _taskRef = _firestore.collection(COLLECTION_REF).withConverter<Task>(
+        fromFirestore: (snapshots, _) =>
+            Task.fromFirestore(snapshots.data()! as DocumentSnapshot<Object?>),
+        toFirestore: (task, _) => task.toJson());
   }
 
-  Future<void> updateTask(Task task) async {
-    await tasksCollection.doc(task.taskId).update(task.toJson());
+  Stream<QuerySnapshot> getTasks() {
+    return _taskRef.snapshots();
   }
 
-  Future<void> deleteTask(String taskId) async {
-    await tasksCollection.doc(taskId).delete();
-  }
+  // Future<void> addTask(Task task) async {
+  //   await tasksCollection.add(task.toJson());
+  // }
 
-  Stream<List<Task>> getTasksStream() {
-    return tasksCollection.snapshots().map(
-          (snapshot) => snapshot.docs
-              .map(
-                (doc) => Task.fromFirestore(doc),
-              )
-              .toList(),
-        );
-  }
+  // Future<void> updateTask(Task task) async {
+  //   await tasksCollection.doc(task.taskId).update(task.toJson());
+  // }
+
+  // Future<void> deleteTask(String taskId) async {
+  //   await tasksCollection.doc(taskId).delete();
+  // }
+
+  // Stream<List<Task>> getTasksStream() {
+  //   return tasksCollection.snapshots().map(
+  //         (snapshot) => snapshot.docs
+  //             .map(
+  //               (doc) => Task.fromFirestore(doc),
+  //             )
+  //             .toList(),
+  //       );
+  // }
 }
