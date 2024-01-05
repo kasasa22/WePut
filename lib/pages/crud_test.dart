68913c9024874cea9bb33c783ad0c3e2a6b7b026
Ambrose Alanda <alandaambrose@gmail.com>
2024-01-05 17:47:28 +0000

    Jan 5, 2024, 8:47 PM

--- lib/pages/crud_test.dart
+++ lib/pages/crud_test.dart
@@ -44,16 +44,19 @@ class _TestState extends State<Test> {
           }
           print(tasks.length);
 
-          return ListView(
-            children: snapshot.data!.docs.map((DocumentSnapshot document) {
-              Map<String, dynamic> data =
-                  document.data()! as Map<String, dynamic>;
-              return ListTile(
-                title: Text(data['title']),
-                subtitle: Text(data['description']),
-              );
-            }).toList(),
-          );
+          return const Center();
+
+          // return ListView(
+          //   children: snapshot.data!.docs.map((DocumentSnapshot document) {
+          //     Map<String, dynamic> data =
+          //         document.data()! as Map<String, Object>;
+          //     return ListTile(
+          //       title: Text(data['title']),
+          //       subtitle: Text(data['description']),
+          //     );
+          //   },
+          //   ).toList(),
+          // );
         },
       ),
     );
