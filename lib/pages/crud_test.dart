1cb25d2ae2bce45feccd45b3f84c8928e8855d7f
Ambrose Alanda <alandaambrose@gmail.com>
2024-01-05 12:17:12 +0000

    Jan 5, 2024, 3:17 PM

--- lib/pages/crud_test.dart
+++ lib/pages/crud_test.dart
@@ -1,15 +1,67 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class Test extends StatefulWidget {
-  const Test({super.key});
+  const Test({Key? key}) : super(key: key);
 
   @override
-  State<Test> createState() => _TestState();
+  _TestState createState() => _TestState();
 }
 
 class _TestState extends State<Test> {
+  final CollectionReference tasksCollection =
+      FirebaseFirestore.instance.collection('tasks');
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('CRUD Test'),
+      ),
+      body: StreamBuilder<QuerySnapshot>(
+        stream: tasksCollection.snapshots(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return CircularProgressIndicator();
+          }
+
+          if (!snapshot.hasData || snapshot.data!.docs.isEmpty) {
+            return Center(
+              child: Text('No tasks available.'),
+            );
+          }
+
+          return ListView.builder(
+            itemCount: snapshot.data!.docs.length,
+            itemBuilder: (context, index) {
+              var task = snapshot.data!.docs[index];
+              return ListTile(
+                title: Text(task['title']),
+                subtitle: Text(task['description']),
+                trailing: IconButton(
+                  icon: Icon(Icons.delete),
+                  onPressed: () => _deleteTask(task.id),
+                ),
+              );
+            },
+          );
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => _addTask(),
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+
+  Future<void> _addTask() async {
+    await tasksCollection.add({
+      'title': 'New Task',
+      'description': 'Description for the new task.',
+    });
+  }
+
+  Future<void> _deleteTask(String taskId) async {
+    await tasksCollection.doc(taskId).delete();
   }
 }
