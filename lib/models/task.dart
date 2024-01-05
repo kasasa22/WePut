c010f5f1199cccbb89e917673367d5cc242bedbc
Ambrose Alanda <alandaambrose@gmail.com>
2024-01-05 13:25:49 +0000

    Jan 5, 2024, 4:25 PM

--- lib/models/task.dart
+++ lib/models/task.dart
@@ -31,41 +31,37 @@ class Task {
     required this.evaluation,
   });
 
-  factory Task.fromFirestore(DocumentSnapshot doc) {
-    Map data = doc.data() as Map;
-    return Task(
-      taskId: doc.id,
-      title: data['title'] ?? '', // Fix the field name typo here
-      description: data['description'] ?? '',
-      dueDate: (data['dueDate'] as Timestamp?) ?? Timestamp.now(),
-      status: data['status'] ?? '',
-      assignedUserId: data['assignedUserId'] ?? '',
-      priority: data['priority'] ?? '',
-      category: data['category'] ?? '',
-      progress: (data['progress'] as int?) ?? 0,
-      comments: List<String>.from(data['comments'] ?? []),
-      startTime: (data['startTime'] as Timestamp?) ?? Timestamp.now(),
-      endTime: (data['endTIme'] as Timestamp?) ?? Timestamp.now(),
-      evaluation: (data['evaluation'] as num?)?.toDouble() ?? 0.0,
-    );
-  }
+  Task.fromJson(Map<String, Object?> json)
+      : this(
+          taskId: json['taskId']! as String,
+          title: json['title']! as String,
+          description: json['description']! as String,
+          dueDate: json['dueDate']! as Timestamp,
+          status: json['status']! as String,
+          assignedUserId: json['assignedUserId']! as String,
+          priority: json['priority']! as String,
+          category: json['category']! as String,
+          progress: json['progress']! as int,
+          comments: json['comments']! as List<String>,
+          startTime: json['startTime']! as Timestamp,
+          endTime: json['endTime']! as Timestamp,
+          evaluation: json['evaluation']! as double,
+        );
 
-  Map<String, dynamic> toJson() {
-    return {
-      'title': title,
-      'description': description,
-      'dueDate': dueDate,
-      'status': status,
-      'assignedUserId': assignedUserId,
-      'priority': priority,
-      'category': category,
-      'progress': progress,
-      'comments': comments,
-      'startTime': startTime,
-      'endTime': endTime,
-      'evaluation': evaluation,
-    };
-  }
+  Map<String, dynamic> toJson() => {
+        'title': title,
+        'description': description,
+        'dueDate': dueDate,
+        'status': status,
+        'assignedUserId': assignedUserId,
+        'priority': priority,
+        'category': category,
+        'progress': progress,
+        'comments': comments,
+        'startTime': startTime,
+        'endTime': endTime,
+        'evaluation': evaluation,
+      };
 
   Task copyWith({
     String? title,
