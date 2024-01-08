31d3fb730f49f9f7d6d615a8d74bb4e39dd91bc4
Ambrose Alanda <alandaambrose@gmail.com>
2024-01-08 10:16:24 +0000

    Jan 8, 2024, 1:16 PM

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -14,30 +14,6 @@ class HomePage extends StatelessWidget {
       appBar: AppBar(
         backgroundColor: Colors.green[700],
         title: const Text("Profile"),
-        actions: [
-          PopupMenuButton(
-            onSelected: (String value) {},
-            itemBuilder: (context) => [
-              const PopupMenuItem(
-                value: "Settings",
-                child: Text("Settings"),
-              ),
-              PopupMenuItem(
-                value: "About",
-                child: IconButton(
-                  onPressed: () {
-                    //sign out the user
-                    FirebaseAuth.instance.signOut();
-                  },
-                  icon: const Icon(
-                    Icons.logout,
-                    size: 20,
-                  ),
-                ),
-              ),
-            ],
-          )
-        ],
       ),
 
       //Drawer
