18142e175059bba15f4cb236c44330f6dddba23d
Ambrose Alanda <alandaambrose@gmail.com>
2024-01-08 11:57:39 +0000

    Jan 8, 2024, 2:57 PM

--- lib/adapters/basic_list_adapter.dart
+++ lib/adapters/basic_list_adapter.dart
@@ -41,6 +41,9 @@ class _ItemTileState extends State<ItemTile> {
   Widget build(BuildContext context) {
     return Column(
       children: [
+        Container(
+          height: 20,
+        ),
         ExpansionTile(
           leading: SizedBox(
             width: 50,
