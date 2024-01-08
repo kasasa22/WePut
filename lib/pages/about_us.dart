4dfca710a6662dac95018a1830ba26824651f048
Ambrose Alanda <alandaambrose@gmail.com>
2024-01-08 10:37:52 +0000

    Jan 8, 2024, 1:37 PM

--- lib/pages/about_us.dart
+++ lib/pages/about_us.dart
@@ -1,77 +1,51 @@
-// ignore_for_file: public_member_api_docs
-
 import 'package:about/about.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-void main() => runApp(const About());
-
-class About extends StatelessWidget {
-  const About({Key? key}) : super(key: key);
+class name extends StatefulWidget {
+  const name({super.key});
 
   @override
+  State<name> createState() => _nameState();
+}
+
+class _nameState extends State<name> {
+  @override
   Widget build(BuildContext context) {
-    final theme = Theme.of(context);
-    final isIos = theme.platform == TargetPlatform.iOS ||
-        theme.platform == TargetPlatform.macOS;
-
-    // ignore: prefer_const_constructors
-    final aboutPage = AboutPage(
-      title: const Text('About'),
-      applicationVersion: 'Version {{ version }}, build #{{ buildNumber }}',
-      applicationDescription: const Text(
-        'This is an example application that shows the usage of the about_page package.',
+    return Scaffold(
+      body: const SingleChildScrollView(
+        child: Column(children: <Widget>[
+          MarkdownPageListTile(
+            filename: 'README.md',
+            title: Text('View Readme'),
+            icon: Icon(Icons.all_inclusive),
+          ),
+          MarkdownPageListTile(
+            filename: 'CHANGELOG.md',
+            title: Text('View Changelog'),
+            icon: Icon(Icons.view_list),
+          ),
+          MarkdownPageListTile(
+            filename: 'LICENSE.md',
+            title: Text('View License'),
+            icon: Icon(Icons.description),
+          ),
+          MarkdownPageListTile(
+            filename: 'CONTRIBUTING.md',
+            title: Text('Contributing'),
+            icon: Icon(Icons.share),
+          ),
+          MarkdownPageListTile(
+            filename: 'CODE_OF_CONDUCT.md',
+            title: Text('Code of conduct'),
+            icon: Icon(Icons.sentiment_satisfied),
+          ),
+          LicensesPageListTile(
+            title: Text('Open source Licenses'),
+            icon: Icon(Icons.favorite),
+          ),
+        ]),
       ),
-      applicationIcon: const FlutterLogo(size: 100),
-      applicationLegalese: 'Copyright © {{ author }}, {{ year }}',
-      children: const <Widget>[
-        MarkdownPageListTile(
-          filename: 'README.md',
-          title: Text('View Readme'),
-          icon: Icon(Icons.all_inclusive),
-        ),
-        MarkdownPageListTile(
-          filename: 'CHANGELOG.md',
-          title: Text('View Changelog'),
-          icon: Icon(Icons.view_list),
-        ),
-        MarkdownPageListTile(
-          filename: 'LICENSE.md',
-          title: Text('View License'),
-          icon: Icon(Icons.description),
-        ),
-        MarkdownPageListTile(
-          filename: 'CONTRIBUTING.md',
-          title: Text('Contributing'),
-          icon: Icon(Icons.share),
-        ),
-        MarkdownPageListTile(
-          filename: 'CODE_OF_CONDUCT.md',
-          title: Text('Code of conduct'),
-          icon: Icon(Icons.sentiment_satisfied),
-        ),
-        LicensesPageListTile(
-          title: Text('Open source Licenses'),
-          icon: Icon(Icons.favorite),
-        ),
-      ],
-    );
-
-    if (isIos) {
-      return CupertinoApp(
-        title: 'About Demo (Cupertino)',
-        home: aboutPage,
-        theme: CupertinoThemeData(
-          brightness: theme.brightness,
-        ),
-      );
-    }
-
-    return MaterialApp(
-      title: 'About Demo (Material)',
-      home: aboutPage,
-      theme: ThemeData(),
-      darkTheme: ThemeData(brightness: Brightness.dark),
     );
   }
 }
