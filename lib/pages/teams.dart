e005c1d3debba4e4a81889a476c7baa079575380
Ambrose Alanda <alandaambrose@gmail.com>
2024-01-09 02:45:56 +0000

    Jan 9, 2024, 5:45 AM

--- lib/pages/teams.dart
+++ lib/pages/teams.dart
@@ -46,6 +46,54 @@ class Teams extends StatelessWidget {
           taskId: "hgfkkj",
           assignmentTime: Timestamp(2, 4),
           completionStatus: "nx,dvhjxj"),
+      Assignment(
+          assignmentId: "njsfd",
+          userId: "hsjvjdt",
+          taskId: "hgfkkj",
+          assignmentTime: Timestamp(2, 4),
+          completionStatus: "nx,dvhjxj"),
+      Assignment(
+          assignmentId: "njsfd",
+          userId: "hsjvjdt",
+          taskId: "hgfkkj",
+          assignmentTime: Timestamp(2, 4),
+          completionStatus: "nx,dvhjxj"),
+      Assignment(
+          assignmentId: "njsfd",
+          userId: "hsjvjdt",
+          taskId: "hgfkkj",
+          assignmentTime: Timestamp(2, 4),
+          completionStatus: "nx,dvhjxj"),
+      Assignment(
+          assignmentId: "njsfd",
+          userId: "hsjvjdt",
+          taskId: "hgfkkj",
+          assignmentTime: Timestamp(2, 4),
+          completionStatus: "nx,dvhjxj"),
+      Assignment(
+          assignmentId: "njsfd",
+          userId: "hsjvjdt",
+          taskId: "hgfkkj",
+          assignmentTime: Timestamp(2, 4),
+          completionStatus: "nx,dvhjxj"),
+      Assignment(
+          assignmentId: "njsfd",
+          userId: "hsjvjdt",
+          taskId: "hgfkkj",
+          assignmentTime: Timestamp(2, 4),
+          completionStatus: "nx,dvhjxj"),
+      Assignment(
+          assignmentId: "njsfd",
+          userId: "hsjvjdt",
+          taskId: "hgfkkj",
+          assignmentTime: Timestamp(2, 4),
+          completionStatus: "nx,dvhjxj"),
+      Assignment(
+          assignmentId: "njsfd",
+          userId: "hsjvjdt",
+          taskId: "hgfkkj",
+          assignmentTime: Timestamp(2, 4),
+          completionStatus: "nx,dvhjxj"),
     ];
     List<Widget> gridAssignments = getGridViewAssignments(listAssignments);
     return Scaffold(
