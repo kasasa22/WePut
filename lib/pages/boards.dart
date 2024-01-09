ffce72ef8d560194212db22c0f23c17658e0c55a
Ambrose Alanda <alandaambrose@gmail.com>
2024-01-09 01:08:21 +0000

    Jan 9, 2024, 4:08 AM

--- lib/pages/boards.dart
+++ lib/pages/boards.dart
@@ -2,8 +2,30 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:maker/components/drawer.dart';
 
-class Boards extends StatelessWidget {
+class Boards extends StatefulWidget {
   const Boards({super.key});
+
+  @override
+  State<Boards> createState() => _BoardsState();
+}
+
+class _BoardsState extends State<Boards> with SingleTickerProviderStateMixin {
+  TabController? _tabController;
+  ScrollController? _scrollController;
+
+  @override
+  void initState() {
+    _tabController = TabController(length: 3, vsync: this);
+    _scrollController = ScrollController();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _scrollController!.dispose();
+    _tabController!.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -40,8 +62,29 @@ class Boards extends StatelessWidget {
       drawer: const MyDrawer(),
 
       //Body
-      body: const Center(
-        child: Text("Boards Page"),
+      body: SingleChildScrollView(
+        scrollDirection: Axis.vertical,
+        child: Column(
+          children: [
+            TabBar(
+              indicatorColor: Colors.black,
+              indicatorSize: TabBarIndicatorSize.tab,
+              indicatorWeight: 4,
+              controller: _tabController,
+              tabs: const [
+                Tab(
+                  icon: Text("Assigned"),
+                ),
+                Tab(
+                  icon: Text("In-Progress"),
+                ),
+                Tab(
+                  icon: Text("Completed"),
+                ),
+              ],
+            )
+          ],
+        ),
       ),
     );
   }
