5b0c73207522a1b4b8b11944c663d824aa49d985
Ambrose Alanda <alandaambrose@gmail.com>
2024-01-10 06:49:38 +0000

    Jan 10, 2024, 9:49 AM

--- lib/pages/tasks.dart
+++ lib/pages/tasks.dart
@@ -104,6 +104,19 @@ class _TasksState extends State<Tasks> with SingleTickerProviderStateMixin {
       //Drawer
       drawer: const MyDrawer(),
 
+      floatingActionButton: FloatingActionButton(
+        heroTag: "fab",
+        backgroundColor: Colors.pink[500],
+        elevation: 3,
+        child: const Icon(
+          Icons.add_task,
+          color: Colors.white,
+        ),
+        onPressed: () {
+          showSheet(context);
+        },
+      ),
+
       //Body
       body: Column(
         children: [
@@ -154,4 +167,363 @@ class _TasksState extends State<Tasks> with SingleTickerProviderStateMixin {
       ),
     );
   }
+
+  void showSheet(context) {
+    TextStyle(color: Colors.white, height: 1.4, fontSize: 16);
+    showModalBottomSheet(
+      context: context,
+      builder: (BuildContext bc) {
+        return Scaffold(
+          resizeToAvoidBottomInset: false,
+          body: SingleChildScrollView(
+            child: Container(
+              color: Colors.white,
+              padding: EdgeInsets.symmetric(vertical: 0, horizontal: 5),
+              child: new Column(
+                mainAxisSize: MainAxisSize.min,
+                children: <Widget>[
+                  Container(
+                    height: 10,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 10.0),
+                    child: Align(
+                      alignment: Alignment.centerLeft,
+                      child: Text(
+                        "Property Type",
+                        style: TextStyle(
+                          color: Colors.grey[700],
+                          fontSize: 16,
+                          fontWeight: FontWeight.w500,
+                        ),
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(10.0),
+                    child: new Container(
+                      width: double.infinity,
+                      height: 40,
+                      decoration: myBoxDecoration(),
+                      child: Stack(
+                        children: [
+                          new Padding(
+                            padding: const EdgeInsets.only(left: 10.0),
+                            child: new Align(
+                              alignment: Alignment.centerLeft,
+                              child: new Text(
+                                "Appartment & Unit",
+                                style: TextStyle(
+                                  color: Colors.black,
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.w500,
+                                ),
+                              ),
+                            ),
+                          ),
+                          new Padding(
+                            padding: const EdgeInsets.only(right: 10.0),
+                            child: new Align(
+                              alignment: Alignment.centerRight,
+                              child: new Icon(
+                                Icons.arrow_drop_down,
+                                color: Colors.grey[700],
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(10.0),
+                    child: Row(
+                      children: <Widget>[
+                        new Flexible(
+                          child: Column(
+                            children: <Widget>[
+                              new Padding(
+                                padding: const EdgeInsets.only(bottom: 10.0),
+                                child: Align(
+                                  alignment: Alignment.centerLeft,
+                                  child: Text(
+                                    "Min Price",
+                                    style: TextStyle(
+                                      color: Colors.grey[700],
+                                    ),
+                                  ),
+                                ),
+                              ),
+                              new Container(
+                                width: double.infinity,
+                                height: 40,
+                                decoration: myBoxDecoration(),
+                                child: Stack(
+                                  children: [
+                                    new Padding(
+                                      padding:
+                                          const EdgeInsets.only(left: 10.0),
+                                      child: new Align(
+                                        alignment: Alignment.centerLeft,
+                                        child: Text("1000"),
+                                      ),
+                                    ),
+                                    new Padding(
+                                      padding: EdgeInsets.only(right: 10.0),
+                                      child: new Align(
+                                        alignment: Alignment.centerRight,
+                                        child: new Icon(
+                                          Icons.arrow_drop_down,
+                                          color: Colors.grey[700],
+                                        ),
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ],
+                          ),
+                          flex: 1,
+                        ),
+                        Container(
+                          width: 40,
+                        ),
+                        new Flexible(
+                          child: Column(
+                            children: <Widget>[
+                              new Padding(
+                                padding: const EdgeInsets.only(bottom: 10.0),
+                                child: Align(
+                                  alignment: Alignment.centerLeft,
+                                  child: Text(
+                                    "Max Price",
+                                    style: TextStyle(
+                                      color: Colors.grey[700],
+                                    ),
+                                  ),
+                                ),
+                              ),
+                              new Container(
+                                width: double.infinity,
+                                height: 40,
+                                decoration: myBoxDecoration(),
+                                child: Stack(
+                                  children: [
+                                    new Padding(
+                                      padding:
+                                          const EdgeInsets.only(left: 10.0),
+                                      child: new Align(
+                                        alignment: Alignment.centerLeft,
+                                        child: Text("5000"),
+                                      ),
+                                    ),
+                                    new Padding(
+                                      padding: EdgeInsets.only(right: 10.0),
+                                      child: new Align(
+                                        alignment: Alignment.centerRight,
+                                        child: new Icon(
+                                          Icons.arrow_drop_down,
+                                          color: Colors.grey[700],
+                                        ),
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ],
+                          ),
+                          flex: 1,
+                        ),
+                      ],
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(10.0),
+                    child: Row(
+                      children: <Widget>[
+                        new Flexible(
+                          child: Column(
+                            children: <Widget>[
+                              new Padding(
+                                padding: const EdgeInsets.only(bottom: 10.0),
+                                child: Align(
+                                  alignment: Alignment.centerLeft,
+                                  child: Text(
+                                    "Min Bedrooms",
+                                    style: TextStyle(
+                                      color: Colors.grey[700],
+                                    ),
+                                  ),
+                                ),
+                              ),
+                              new Container(
+                                width: double.infinity,
+                                height: 40,
+                                decoration: myBoxDecoration(),
+                                child: Stack(
+                                  children: [
+                                    new Padding(
+                                      padding:
+                                          const EdgeInsets.only(left: 10.0),
+                                      child: new Align(
+                                        alignment: Alignment.centerLeft,
+                                        child: Text("1000"),
+                                      ),
+                                    ),
+                                    new Padding(
+                                      padding: EdgeInsets.only(right: 10.0),
+                                      child: new Align(
+                                        alignment: Alignment.centerRight,
+                                        child: new Icon(
+                                          Icons.arrow_drop_down,
+                                          color: Colors.grey[700],
+                                        ),
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ],
+                          ),
+                          flex: 1,
+                        ),
+                        Container(
+                          width: 40,
+                        ),
+                        new Flexible(
+                          child: Column(
+                            children: <Widget>[
+                              new Padding(
+                                padding: const EdgeInsets.only(bottom: 10.0),
+                                child: Align(
+                                  alignment: Alignment.centerLeft,
+                                  child: Text(
+                                    "Max Bedrooms",
+                                    style: TextStyle(
+                                      color: Colors.grey[700],
+                                    ),
+                                  ),
+                                ),
+                              ),
+                              new Container(
+                                width: double.infinity,
+                                height: 40,
+                                decoration: myBoxDecoration(),
+                                child: Stack(
+                                  children: [
+                                    new Padding(
+                                      padding:
+                                          const EdgeInsets.only(left: 10.0),
+                                      child: new Align(
+                                        alignment: Alignment.centerLeft,
+                                        child: Text("5000"),
+                                      ),
+                                    ),
+                                    new Padding(
+                                      padding: EdgeInsets.only(right: 10.0),
+                                      child: new Align(
+                                        alignment: Alignment.centerRight,
+                                        child: new Icon(
+                                          Icons.arrow_drop_down,
+                                          color: Colors.grey[700],
+                                        ),
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ],
+                          ),
+                          flex: 1,
+                        ),
+                      ],
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 10.0),
+                    child: Align(
+                      alignment: Alignment.centerLeft,
+                      child: Text(
+                        "Min Land size msq (optional)",
+                        style: TextStyle(
+                          color: Colors.grey[700],
+                          fontSize: 16,
+                          fontWeight: FontWeight.w500,
+                        ),
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(10.0),
+                    child: new Container(
+                      width: double.infinity,
+                      height: 40,
+                      decoration: myBoxDecoration(),
+                      child: Stack(
+                        children: [
+                          new Padding(
+                            padding: const EdgeInsets.only(left: 10.0),
+                            child: new Align(
+                              alignment: Alignment.centerLeft,
+                              child: new Text(
+                                "400",
+                                style: TextStyle(
+                                  color: Colors.black,
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.w500,
+                                ),
+                              ),
+                            ),
+                          ),
+                          new Padding(
+                            padding: const EdgeInsets.only(right: 10.0),
+                            child: new Align(
+                              alignment: Alignment.centerRight,
+                              child: new Icon(
+                                Icons.arrow_drop_down,
+                                color: Colors.grey[700],
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 10.0),
+                    child: SizedBox(
+                      width: double.infinity,
+                      child: ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: Colors.black,
+                        ),
+                        child: Text(
+                          "SEARCH",
+                          style: TextStyle(
+                            color: Colors.white,
+                          ),
+                        ),
+                        onPressed: () {},
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        );
+      },
+    );
+  }
+}
+
+BoxDecoration myBoxDecoration() {
+  return BoxDecoration(
+    border: Border.all(
+      color: Colors.grey[300]!,
+      width: 1.0,
+    ),
+    borderRadius: BorderRadius.all(Radius.circular(5.0) //
+        ),
+  );
 }
