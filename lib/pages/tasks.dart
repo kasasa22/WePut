d253c7e9c64a2bbadee1400eacc0612b8178c8a5
Ambrose Alanda <alandaambrose@gmail.com>
2024-01-10 07:47:16 +0000

    Jan 10, 2024, 10:47 AM

--- lib/pages/tasks.dart
+++ lib/pages/tasks.dart
@@ -7,7 +7,7 @@ import '../adapters/task_list_adapter.dart';
 import '../models/task.dart';
 
 class Tasks extends StatefulWidget {
-  const Tasks({super.key});
+  const Tasks({Key? key}) : super(key: key);
 
   @override
   State<Tasks> createState() => _TasksState();
@@ -16,6 +16,7 @@ class Tasks extends StatefulWidget {
 class _TasksState extends State<Tasks> with SingleTickerProviderStateMixin {
   TabController? _tabController;
   ScrollController? _scrollController;
+  late DateTime? _selectedDate; // Declare _selectedDate here
 
   @override
   void initState() {
@@ -30,10 +31,6 @@ class _TasksState extends State<Tasks> with SingleTickerProviderStateMixin {
     _tabController!.dispose();
     super.dispose();
   }
-
-  late BuildContext _scaffoldCtx;
-
-  late PersistentBottomSheetController sheetController;
 
   @override
   Widget build(BuildContext context) {
@@ -88,7 +85,7 @@ class _TasksState extends State<Tasks> with SingleTickerProviderStateMixin {
                 value: "About",
                 child: IconButton(
                   onPressed: () {
-                    //sign out the user
+                    // sign out the user
                     FirebaseAuth.instance.signOut();
                   },
                   icon: const Icon(
@@ -101,7 +98,7 @@ class _TasksState extends State<Tasks> with SingleTickerProviderStateMixin {
           )
         ],
       ),
-      //Drawer
+      // Drawer
       drawer: const MyDrawer(),
 
       floatingActionButton: FloatingActionButton(
@@ -117,7 +114,7 @@ class _TasksState extends State<Tasks> with SingleTickerProviderStateMixin {
         },
       ),
 
-      //Body
+      // Body
       body: Column(
         children: [
           TabBar(
@@ -162,7 +159,7 @@ class _TasksState extends State<Tasks> with SingleTickerProviderStateMixin {
                 )
               ],
             ),
-          )
+          ),
         ],
       ),
     );
@@ -172,120 +169,143 @@ class _TasksState extends State<Tasks> with SingleTickerProviderStateMixin {
     showModalBottomSheet(
       context: context,
       builder: (BuildContext bc) {
-        return Scaffold(
-          resizeToAvoidBottomInset: false,
-          body: SingleChildScrollView(
-            child: Container(
-              color: Colors.white,
-              padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 15),
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
+        return TaskSheet(
+          selectedDate: _selectedDate, // Pass _selectedDate to TaskSheet
+        );
+      },
+    );
+  }
+}
+
+class TaskSheet extends StatefulWidget {
+  DateTime? selectedDate; // Declare selectedDate as a parameter
+
+  TaskSheet({Key? key, required this.selectedDate}) : super(key: key);
+
+  @override
+  _TaskSheetState createState() => _TaskSheetState();
+}
+
+class _TaskSheetState extends State<TaskSheet> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      body: SingleChildScrollView(
+        child: Container(
+          color: Colors.white,
+          padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 15),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              const Padding(
+                padding: EdgeInsets.only(bottom: 10.0),
+                child: Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    "Create New Task",
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ),
+              const TextField(
+                decoration: InputDecoration(labelText: 'Title'),
+              ),
+              const SizedBox(height: 10),
+              const TextField(
+                decoration: InputDecoration(labelText: 'Description'),
+              ),
+              const SizedBox(height: 10),
+              Row(
                 children: <Widget>[
-                  Padding(
-                    padding: const EdgeInsets.only(bottom: 10.0),
-                    child: Align(
-                      alignment: Alignment.centerLeft,
-                      child: Text(
-                        "Create New Task",
-                        style: TextStyle(
-                          color: Colors.black,
-                          fontSize: 20,
-                          fontWeight: FontWeight.bold,
+                  Expanded(
+                    child: InkWell(
+                      onTap: () async {
+                        // Show date picker and update the selected date
+                        DateTime? pickedDate = await showDatePicker(
+                          context: context,
+                          initialDate: DateTime.now(),
+                          firstDate: DateTime.now(),
+                          lastDate: DateTime(2101),
+                        );
+
+                        if (pickedDate != null &&
+                            pickedDate != widget.selectedDate) {
+                          setState(() {
+                            widget.selectedDate = pickedDate;
+                          });
+                        }
+                      },
+                      child: InputDecorator(
+                        decoration: const InputDecoration(
+                          labelText: 'Due Date',
+                          border: OutlineInputBorder(),
+                        ),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: <Widget>[
+                            Text(
+                              widget.selectedDate != null
+                                  ? "${widget.selectedDate!.toLocal()}"
+                                      .split(' ')[0]
+                                  : 'Select Date',
+                            ),
+                            const Icon(Icons.calendar_today),
+                          ],
                         ),
                       ),
                     ),
                   ),
-                  TextField(
-                    decoration: InputDecoration(labelText: 'Title'),
-                  ),
-                  const SizedBox(height: 10),
-                  TextField(
-                    decoration: InputDecoration(labelText: 'Description'),
-                  ),
-                  const SizedBox(height: 10),
-                  Row(
-                    children: <Widget>[
-                      Expanded(
-                        child: InkWell(
-                          onTap: () {
-                            // Show date picker and update the selected date
-                            // Implement your logic here
-                          },
-                          child: InputDecorator(
-                            decoration: InputDecoration(
-                              labelText: 'Due Date',
-                              border: OutlineInputBorder(),
-                            ),
-                            child: Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                              children: <Widget>[
-                                Text('Selected Date'),
-                                Icon(Icons.calendar_today),
-                              ],
-                            ),
-                          ),
-                        ),
+                  const SizedBox(width: 10),
+                  Expanded(
+                    child: DropdownButtonFormField<String>(
+                      decoration: const InputDecoration(
+                        labelText: 'Priority',
+                        border: OutlineInputBorder(),
                       ),
-                      const SizedBox(width: 10),
-                      Expanded(
-                        child: DropdownButtonFormField<String>(
-                          decoration: InputDecoration(
-                            labelText: 'Priority',
-                            border: OutlineInputBorder(),
-                          ),
-                          value:
-                              'High', // Initial value, you can change it as needed
-                          items: ['High', 'Medium', 'Low']
-                              .map((priority) => DropdownMenuItem(
-                                    value: priority,
-                                    child: Text(priority),
-                                  ))
-                              .toList(),
-                          onChanged: (value) {
-                            // Handle the selected priority
-                            // Implement your logic here
-                          },
-                        ),
-                      ),
-                    ],
-                  ),
-                  const SizedBox(height: 20),
-                  SizedBox(
-                    width: double.infinity,
-                    child: ElevatedButton(
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor: Colors.blue[700],
-                      ),
-                      child: const Text(
-                        "Create Task",
-                        style: TextStyle(
-                          color: Colors.white,
-                        ),
-                      ),
-                      onPressed: () {
-                        // Handle the task creation logic here
-                        // You can retrieve data from the text fields and save it
-                        // to your database or perform any other necessary actions.
+                      value: 'High',
+                      items: ['High', 'Medium', 'Low']
+                          .map((priority) => DropdownMenuItem(
+                                value: priority,
+                                child: Text(priority),
+                              ))
+                          .toList(),
+                      onChanged: (value) {
+                        // Handle the selected priority
+                        // Implement your logic here
                       },
                     ),
                   ),
                 ],
               ),
-            ),
+              const SizedBox(height: 20),
+              SizedBox(
+                width: double.infinity,
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.blue[700],
+                  ),
+                  child: const Text(
+                    "Create Task",
+                    style: TextStyle(
+                      color: Colors.white,
+                    ),
+                  ),
+                  onPressed: () {
+                    // Handle the task creation logic here
+                    // You can retrieve data from the text fields and save it
+                    // to your database or perform any other necessary actions.
+                  },
+                ),
+              ),
+            ],
           ),
-        );
-      },
-    );
-  }
-
-  BoxDecoration myBoxDecoration() {
-    return BoxDecoration(
-      border: Border.all(
-        color: Colors.grey[300]!,
-        width: 1.0,
+        ),
       ),
-      borderRadius: const BorderRadius.all(Radius.circular(5.0) //
-          ),
     );
   }
 }
