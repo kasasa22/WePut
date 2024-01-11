42f5a0810abe708051070d825aa1b246aa67a098
Ambrose Alanda <alandaambrose@gmail.com>
2024-01-11 04:40:23 +0000

    Jan 11, 2024, 7:40 AM

--- lib/pages/tasks.dart
+++ lib/pages/tasks.dart
@@ -48,21 +48,21 @@ class _TasksState extends State<Tasks> with SingleTickerProviderStateMixin {
   List<Task> completedTasks = [];
 
   List<Task> items = [
-    Task(
-      taskId: '1',
-      title: 'Complete Flutter App',
-      description: 'Finish building the Flutter app for the project.',
-      dueDate: Timestamp.now(),
-      status: 'In-Progress',
-      assignedUserId: 'user1',
-      priority: 'High',
-      category: 'Development',
-      progress: 50,
-      comments: ['Comment 1', 'Comment 2'],
-      startTime: Timestamp.now(),
-      endTime: Timestamp.now(),
-      evaluation: 4.5,
-    ),
+    // Task(
+    //   taskId: '1',
+    //   title: 'Complete Flutter App',
+    //   description: 'Finish building the Flutter app for the project.',
+    //   dueDate: Timestamp.now(),
+    //   status: 'In-Progress',
+    //   assignedUserId: 'user1',
+    //   priority: 'High',
+    //   category: 'Development',
+    //   progress: 50,
+    //   comments: ['Comment 1', 'Comment 2'],
+    //   startTime: Timestamp.now(),
+    //   endTime: Timestamp.now(),
+    //   evaluation: 4.5,
+    // ),
 
     // Add more tasks as needed
   ];
