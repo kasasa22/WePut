1bdfc4c2b45ea59fa7c2feacb2554f9455bfabf1
Ambrose Alanda <alandaambrose@gmail.com>
2024-01-11 13:29:45 +0000

    Jan 11, 2024, 4:29 PM

--- lib/pages/dashboard.dart
+++ lib/pages/dashboard.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: avoid_print
+
 import 'package:animate_do/animate_do.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
@@ -139,6 +141,9 @@ class _DashboardState extends State<Dashboard> {
   @override
   Widget build(BuildContext context) {
     name = name;
+
+    print("User Name: ---------");
+    print(name);
     return Scaffold(
       backgroundColor: Colors.white,
       appBar: AppBar(
