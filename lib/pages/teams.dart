133c24c0d22b4e53cf17241093d51dd24a2b10d7
Ambrose Alanda <alandaambrose@gmail.com>
2024-01-11 03:20:57 +0000

    Jan 11, 2024, 6:20 AM

--- lib/pages/teams.dart
+++ lib/pages/teams.dart
@@ -44,7 +44,7 @@ class _TeamsState extends State<Teams> {
             "----------------------------------------------------------------------------------------");
         Assignment assignment =
             Assignment.fromJson(document.data() as Map<String, dynamic>);
-        listAssignments.add(assignment);
+        //  listAssignments.add(assignment);
       }
 
       // Print the assignments for debugging
